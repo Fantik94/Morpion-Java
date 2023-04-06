f834ce1ce32dea6d4d75a9ba4da6c1ec606c0339
Fantik94 <ringlerbaptiste@gmail.com>
2023-04-06 16:36:26 +0200

    Update Plateau.java

--- morpion/src/hitema/java/plateau/Plateau.java
+++ morpion/src/hitema/java/plateau/Plateau.java
@@ -13,12 +13,19 @@ public class Plateau {
     }
 
     public void afficher() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(grille[i][j] + " ");
+        System.out.println("   0   1   2");
+        System.out.println(" ┌───┬───┬───┐");
+        for (int i = 0; i < 3; ++i) {
+            System.out.print(i + "│");
+            for (int j = 0; j < 3; ++j) {
+                System.out.print(" " + grille[i][j] + " │");
             }
             System.out.println();
+            if (i < 2) {
+                System.out.println(" ├───┼───┼───┤");
+            }
         }
+        System.out.println(" └───┴───┴───┘");
     }
 
     public void placer(int x, int y, char symbole) {
